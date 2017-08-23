1f27a4538f456f5c32af6e2b755fe7cd35669a08
Taifu <752761705@qq.com>
2017-08-23 12:34:03 +0800

    first commit

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ window.onload = function(){
 }
 
 
+
+
 function changeHash(form){
     if(location.hash.substr(1)==form.search.value){
         Location.reload();
@@ -19,9 +21,7 @@ function locationParameterChanged() {
     }
     document.getElementsByTagName("input")[0].value=key;
     loading(true);
-    window.videos = panc(key);
-    loading(false);
-    setIframe(videos);
+    panc(key);
 }
 function loading(is){           
     var loadingDoc = document.getElementById("loading");
@@ -47,21 +47,33 @@ function setIframe(videos) {
         var div= wrapper.children[1];
         var url = videos[i].url;
         url = url.substr(url.indexOf("http:")+5);
-        div.firstElementChild.src=url;
+        div.firstElementChild.dataset.src=url;
+        if(i==0){
+            div.firstElementChild.src=url;
+        }
+        addVideosHandler(div.firstElementChild);
         document.body.append(wrapper);
     }
 }
 function panc(key){
-    var videos=[];
-    if(key=="#记忆大师"){
-        videos = [{name:"记忆大师",url:"https://pan.baidu.com/share/link?uk=107049535&shareid=1591164568"}];
-    }else if(key=="#绝世高手"){
-        videos = [{name:"绝世高手",url:"https://pan.baidu.com/share/link?uk=1177627517&shareid=1395519671"}];
-    }else{
-        
-        videos = getVideos(key);
+    var url = encodeURI("//charon-node.herokuapp.com/cross?api=https://www.panc.cc/s/"+key+"/td_1");
+    try{
+        var error = function(){
+            var videos = [];
+            window.videos = videos;
+            loading(false);
+            setIframe(videos);
+        }
+        ajax(url,function(html){
+            var videos = parseHref(html,".a_url");
+            window.videos = videos;
+            loading(false);
+            setIframe(videos);
+        },error,"PUT",null);
+
+    }catch(e){
+        error();
     }
-    return videos;
 
 
     function getVideos(key){
@@ -96,4 +108,70 @@ function load(uri,fn){
 
 function getURLParameter(name) {
   return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+}
+
+
+function isElementInViewport (el) {
+
+    //special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
+}
+function onVisibilityChange(el, callback) {
+    var old_visible;
+    return function () {
+        var visible = isElementInViewport(el);
+        if (visible != old_visible) {
+            old_visible = visible;
+            if (typeof callback == 'function') {
+                callback();
+            }
+        }
+    }
+}
+function addVideosHandler(iframe){
+    var handler = onVisibilityChange(iframe, function() {
+        if(!iframe.src){
+            iframe.src = iframe.dataset.src;
+        }
+    });
+    if (window.addEventListener) {
+        addEventListener('DOMContentLoaded', handler, false); 
+        addEventListener('load', handler, false); 
+        addEventListener('scroll', handler, false); 
+        addEventListener('resize', handler, false); 
+    } else if (window.attachEvent)  {
+        attachEvent('onDOMContentLoaded', handler); // IE9+ :(
+        attachEvent('onload', handler);
+        attachEvent('onscroll', handler);
+        attachEvent('onresize', handler);
+    }
+}
+
+function ajax(uri,fn,error,method,data){
+    var request = new XMLHttpRequest();
+    request.open(method||"GET", uri);
+    try{
+      request.send(data||null);
+    }catch(e){
+        error();
+    }
+    request.onload = function(e) {
+        if (this.status == 200) {
+          fn(this.response,error);
+        }else{
+          error();
+        }
+    };
+    request.onerror = error;
 }
