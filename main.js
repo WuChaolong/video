e8eed7bd950a9843ad0b5e99c9cf98ff8ec3eef8
Taifu <752761705@qq.com>
2017-08-31 09:43:10 +0800

    more

--- main.js
+++ main.js
@@ -28,63 +28,131 @@ function loading(is,id){
     var loadingDoc = document.getElementById(id?id:"pancLoading");
     loadingDoc.style.display=is?"block":"none";
 }
+function confirm(id,isImportant){
+    try{
+        
+        var template = document.getElementById(id).innerHTML;
+        var confirm;
+        if(isImportant){
 
+            confirm = document.getElementById("confirm2");
+        }else{
+
+            confirm = document.getElementById("confirm");
+        }
+        confirm.innerHTML = template;
+        if(id=="moreTemplate"){
+            confirm.id = "confirm2";
+        }
+    }catch(e){
+        
+    }
+    
+}
 function panduoduo(key){
     var host = "panduoduo";
+    window[host+"Videos"] = [];
     var success = function(html){
         var videos = parseList(html,"a[href^='/r/']");
-        window[host] = videos;
-        loading(false,host+"Loading");
-
-        function parseList(html,select){
-            var videos = [];
-            var el = document.createElement( 'html' );
-            el.innerHTML = html;
-
-            var as = el.querySelectorAll(select);
-            el = null;
-            for(var i = 0;i<as.length;i++){
-                if(i>10){
-                    break;
-                }
-                var video = {ref:host,name:as[i].innerHTML,url:"http://www.panduoduo.net"+as[i].pathname};
-                videos.push(video);
-                fetchDetal(video,i);
-
-            }
-            return videos;
+        if(!videos||videos.length===0){
+            confirm("noneTemplate");
+            return;
         }
-        function fetchDetal(video,i){
-            var success = function(html){
-
-                video["url"] = parseDetail(html,"a.dbutton2");
-                loading(false,host+"Loading");
-                function parseDetail(html,select){
-                    var href = html.substring(html.indexOf('"dbutton2" href="')+'"dbutton2" href="'.length,html.indexOf('" rel="nofollow">点击去百度云盘下载资源</a>'));
-                    var url = getURLParameter("url",href);
-                    return url;
-                }
-                setIframe([video],i===0);
-            }
-            fetch("panduoduo",video.url,success);
-
+        window[host+"Videos"] = videos;
+        confirm("moreTemplate");
+        if(!window.showVideos){
+            window.showVideos = [];
+            panduoduoSetIframe(window[host+"Videos"],3,window.showVideos)
         }
+        
     };
     fetch(host,"http://www.panduoduo.net/s/comb/n-"+key+"&ty-bd&f-f4",success);
     
+    function parseList(html,select){
+        var videos = [];
+        var el = document.createElement( 'html' );
+        el.innerHTML = html;
+
+        var as = el.querySelectorAll(select);
+        el = null;
+        for(var i = 0;i<as.length;i++){
+//             if(i>10){
+//                 break;
+//             }
+            var video = {ref:host,name:as[i].innerHTML,url:"http://www.panduoduo.net"+as[i].pathname};
+            videos.push(video);
+//                 fetchDetal(video,i===0);
+            
+        }
+        return videos;
+    }
+    function fetchDetal(video,isSrc){
+        var success = function(html){
+
+            video["url"] = parseDetail(html,"a.dbutton2");
+            confirm("moreTemplate");
+            function parseDetail(html,select){
+                var href = html.substring(html.indexOf('"dbutton2" href="')+'"dbutton2" href="'.length,html.indexOf('" rel="nofollow">点击去百度云盘下载资源</a>'));
+                var url = getURLParameter("url",href);
+                return url;
+            }
+            setIframe([video],isSrc);
+        }
+        fetch("panduoduo",video.url,success);
+
+    }
+    window.panduoduoSetIframe = function (panduoduoVideo,length,showVideos){
+        var videos = panduoduoVideo.splice(0,length);
+        videos.forEach(function (item, index, array) {
+            showVideos.push(item);
+            fetchDetal(item,index===0);
+        });
+    }
+}
+
+function ShowMore(){
+
+    var length = window.showVideos.length;
+//         window.pancVideos.splice(0,length);
+    var thanPanc = window.pancVideos.length - length;
+    var PancNum = thanPanc>0?length:window.pancVideos.length;
+    var PanduoduoNum = thanPanc<0?-thanPanc:0;
+    if(window.pancVideos.length){
+        pancSetIframe(window.pancVideos,PancNum,window.showVideos);
+    }
+    if(PanduoduoNum>0){
+        panduoduoSetIframe(window.panduoduoVideos,PanduoduoNum,window.showVideos);
+    }
+    if(window.pancVideos.length===0&&window.panduoduoVideos.length===0){
+        confirm("noneTemplate",true);
+    }
 }
 function panc(key){
 
     var host = "panc";
+    window[host+"Videos"] = [];
     var success = function(html){
         var videos = parseHref(html,".a_url");
-        window[host] = videos;
-        loading(false,host+"Loading");
-        setIframe(videos,true,true);
+        if(!videos||videos.length===0){
+            confirm("noneTemplate");
+            return;
+        }
+        window[host+"Videos"] = videos;
+        
+        confirm("moreTemplate");
+//         setIframe(videos,true,true);
+        if(!window.showVideos){
+            window.showVideos = [];
+            pancSetIframe(window[host+"Videos"],3,window.showVideos);
+        }
     }
     fetch(host,"https://www.panc.cc/s/"+key+"/td_1",success);
 
-
+    window.pancSetIframe = function (pancVideos,length){
+        var videos = pancVideos.splice(0,length);
+        window.showVideos = window.showVideos.concat(videos);
+        setIframe(videos,true,true);
+    }
     function parseHref(html,select){
         var videos = [];
         var el = document.createElement( 'html' );
@@ -99,15 +167,16 @@ function panc(key){
 }
 
 function fetch(host,api,success){
-    loading(true,host+"Loading");
+
+    confirm("loadingTemplate");
     var url = encodeURI("//charon-node.herokuapp.com/fetch");
 //     var url = encodeURI("http://127.0.0.1:8888/fetch");
     var data = JSON.stringify({crossUrl:api});
     try{
         var error = function(){
             var videos = [];
-            loading(false,host+"Loading");
-            setIframe(videos,true,true);
+            confirm("noneTemplate");
+//             setIframe(videos,true,true);
         }
         ajax(url,success,error,"POST",data);
 
@@ -120,15 +189,7 @@ function fetch(host,api,success){
 
 function setIframe(videos,isSrc,isNone) {
     var template = document.getElementById("videoTemplate").innerHTML;
-    if(isNone&&(!videos||!videos.length)){
-        template = document.getElementById("noneTemplate").innerHTML
 
-        var wrapper= document.createElement('div');
-        wrapper.innerHTML= template;
-
-        document.body.append(wrapper);
-        return;
-    }
     var dataBox =  document.getElementById("data");
     for(var i=0;i<videos.length;i++){
         var wrapper= document.createElement('div');
@@ -233,3 +294,4 @@ function ajax(uri,fn,error,method,data){
       error();
     };
 }
+
