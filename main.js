157684546acc2ac157d637722030c5910f883fcc
Taifu <752761705@qq.com>
2017-12-18 19:27:55 +0800

    isit

--- main.js
+++ main.js
@@ -1375,6 +1375,8 @@ function isItClick(isIt){
     storageItTrue(key,isIt,!isIt.checked);
 
     isIt.classList.add("checked");
+    var share = document.querySelector(".share-other");
+    share.setAttribute("checked",isIt.checked);
 }
 
 function storageItTrue(key,isIt,remove){
@@ -1427,5 +1429,5 @@ function loadShare(key){
     importCSS("https://overtrue.github.io/share.js/dist/css/share.min.css");
     window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"1","bdSize":"32"},"share":{}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='https://wuchaolong.github.io/baiduShare/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];
     
-//     importCSS("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css","awesome");
+    importCSS("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css","awesome");
 }
