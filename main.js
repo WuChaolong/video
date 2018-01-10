715c0dbc433eb2dfa34b277bcdd454db0ee9369a
Taifu <752761705@qq.com>
2018-01-11 02:19:24 +0800

    bug

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
 window.onload = function(){
     
 }
-String.prototype.format = function()
-{
-    var args = arguments;
-    return this.replace(/\{(\d+)\}/g,                
-        function(m,i){
-            return args[i];
-        });
-}
+
 var config = {
 //     nodeUrl:"http://127.0.0.1:8888/"
     nodeUrl:"//charon-node.herokuapp.com/"
@@ -23,41 +16,16 @@ var config = {
         }
     }
     ,feedback:{
-        "zh-CN":"<span>反馈意见?赏1分钱,支付宝留言.</span>"
-        ,"default":"<span>Feedback?PayPal message.</span>"
+        "zh-CN":'<a href="#donate" onclick="showAlipay()"><span>反馈意见?赏1分钱,支付宝留言.</span></a>'
+        ,"default":'<a href="#donate" onclick="showPayPal()"><span>Feedback?PayPal message me.</span></a>'
         ,string:function(){
             return this[config.userLang]||this.default;
         }
     }
 };
 
-function setHeight(input,height){
-        input.style.height = height;
-}
 
-function setFontSize(input,maxLength,fontSize){
-    function min(x,y){
-        return x-y<0?x:y;
-    }
-    function max(x,y){
-        return x-y<0?y:x;
-    }
-    if(!fontSize){
-        var fontSize = max(min(window.innerWidth,window.innerHeight)/maxLength,16)+"px";
-
-    }
-    input.style.fontSize = fontSize;
-}
-
-function changeHash(form){
-    if(location.hash.substr(1)==form.search.value){
-        Location.reload();
-    }else{
-
-        location.hash = "#"+form.search.value;
-    }
-    return false;
-}
+//         alert(config.userLang);
 function locationParameterChanged() {
     var key = config.key;
     var input = document.getElementsByTagName("input")[0];
@@ -88,7 +56,10 @@ function locationParameterChanged() {
     if(inIframe()){
         document.querySelector(".search-form").style.display = "none";
         document.querySelector("#searchBottom").style.display = "none";
-        document.querySelector("#data").classList.add("iniframe");
+        if(!mobilecheck){
+            document.querySelector("#data").classList.add("iniframe");
+        }
+        
     }else{
         input.onfocus = function(){
 
@@ -97,6 +68,9 @@ function locationParameterChanged() {
         }
     }
     
+    if(config.userLang=="zh-CN"){
+        document.body.classList.add("zh-CN");
+    }
     document.getElementsByTagName("form")[0].onsubmit = function(){
          setHeight(input,"15%");
 //          setFontSize(input,100,"1.5em");
@@ -129,8 +103,13 @@ function locationParameterChanged() {
 
     input.value=key;
     document.getElementsByTagName("title")[0].innerText=key+" in pan.baidu.com sharing";
-    setMessageForm(key);
-    syncIsIt().getBykey();
+//     setMessageForm(key);
+    try{
+        var self = syncIsIt().getBykey(key);
+        self.setLocalIframe(self.data);
+    }catch(e){
+        console.log(e);
+    }
     window.panc=pancFetcher(key);
     window.panduoduo=panduoduoFetcher(key);
     magnetFetcher(key);
@@ -153,7 +132,33 @@ function locationParameterChanged() {
     }
 //     setMagnetTech();
 }
+function setHeight(input,height){
+        input.style.height = height;
+}
 
+function setFontSize(input,maxLength,fontSize){
+    function min(x,y){
+        return x-y<0?x:y;
+    }
+    function max(x,y){
+        return x-y<0?y:x;
+    }
+    if(!fontSize){
+        var fontSize = max(min(window.innerWidth,window.innerHeight)/maxLength,16)+"px";
+
+    }
+    input.style.fontSize = fontSize;
+}
+
+function changeHash(form){
+    if(location.hash.substr(1)==form.search.value){
+        Location.reload();
+    }else{
+
+        location.hash = "#"+form.search.value;
+    }
+    return false;
+}
 function setMoreFetcher(button){
     button.style.display="none";
     var key = config.key;
@@ -815,12 +820,16 @@ function magnetFetcher(key){
                     var name = els[i].querySelector("a[name='file_title'").innerHTML;
                     var url = els[i].querySelector("a[href^='magnet:']").href;
                     var video = {ref:"magnet",name:name,url:url};
-                    var func = video.name.length<70?"unshift":"push";
-                    videos[func](video);
+                    videos.push(video);
         //                 fetchDetal(video,i===0);
+                    
 
                 }
-                
+                for(var i=0;i<videos.length;i++){
+                    if(videos[i].name.length>70){
+                        videos.move(i, videos.length-1)
+                    }
+                }
                 html = el = els = null;
                 return videos;
             }
@@ -866,7 +875,7 @@ function importScript (sSrc, fOnload) {
       document.body.appendChild(oScript);
   }
   oScript.src = sSrc;
-  oScript.abort = ()=>{
+  oScript.abort = function(){
     return trigger(oScript,'error');
   };
   return oScript;
@@ -880,7 +889,7 @@ function trigger(element,event){
     return;
   }
   if ( element.dispatchEvent ){
-    let evt = document.createEvent('Events');// initEvent接受3个参数
+    var evt = document.createEvent('Events');// initEvent接受3个参数
     evt.initEvent(event, true, true);
     element.dispatchEvent(evt);
   }else if ( element.fireEvent ){ //IE
@@ -1327,10 +1336,11 @@ function syncIsIt(key,data){
     var self = {};
 
     self.key = key|| config.key;
-    self.data = data|| JSON.parse(localStorage.getItem(key)||"{}");
+    self.data = data|| JSON.parse(localStorage.getItem(self.key)||"{}");
 
     self.uri = "https://postgrest-taifu.herokuapp.com/testisit3";
     self.getBykey = function(key){
+
         var key = (key||self.key);
         var method = "get";
         var uri = self.uri+"?order=install_date.desc&key=eq."+key;
@@ -1340,9 +1350,10 @@ function syncIsIt(key,data){
             setLocal(key,data);
         }
         ajax(uri,fn,null,method,null);
+        return self;
     }
     self.setIframe = function (data,id,setKey){
-        var data = JSON.parse(data);
+        var data = (typeof data === "string")?JSON.parse(data):data;
         data.map(function(o){
 
             var video = JSON.parse(o.video);
@@ -1351,6 +1362,13 @@ function syncIsIt(key,data){
                 setIframe(video,true,null,null,id);
             }
         });
+    }
+    self.setLocalIframe = function(data){
+        for(var key in data){
+            var video = data[key];
+            video.key = key;
+            setIframe(video,true,null,null);
+        }
     }
     self.getAll = function(){
         var method = "get";
@@ -1410,7 +1428,7 @@ function isItClick(isIt){
 //     }
     var key = isIt.dataset.key||config.key;
     
-    storageItTrue(key,isIt);
+    storageItTrue(key,isIt,!isIt.checked);
 //     isIt.disabled = true;
     isIt.classList.add("checked");
     var share = document.querySelector(".share-other");
@@ -1428,7 +1446,15 @@ function storageItTrue(key,isIt,remove){
         sync.delete(key,video);
     }else{
         sync.add(key,video);
+        moveItemToTop(isIt);
     }
+}
+function moveItemToTop(isIt){
+    var videoItem = closest(isIt, ".video-item");
+    var dataD = videoItem.parentNode;
+//     videoItem = dataD.removeChild(videoItem);
+    dataD.insertBefore(videoItem,dataD.childNodes[0]);
+    document.querySelector('a[href="#data"]').click();
 }
 function isItTrue(isIt){
     var key = config.key;
@@ -1439,7 +1465,19 @@ function isItTrue(isIt){
     }
     return false;
 }
-
+function closest(el, selector, stopSelector) {
+  var retval = null;
+  while (el) {
+    if (el.matches(selector)) {
+      retval = el;
+      break
+    } else if (stopSelector && el.matches(stopSelector)) {
+      break
+    }
+    el = el.parentElement;
+  }
+  return retval;
+}
 
 function importCSS(href,cssId){
     var cssId = cssId||'myCss';  // you could encode the css path itself to generate id..
@@ -1490,7 +1528,21 @@ function addDonate(){
     var donateD = document.getElementById("donate");
     var spanHtml = config.feedback.string();
     donateD.innerHTML = donateTemplate.innerHTML+spanHtml;
+//     donateD.querySelector("span").click();
     donateTemplate = donateD = null;
+
+
+    
+}
+
+function showAlipay(){
+    var iframe = document.querySelector("#donate iframe");
+    (iframe.contentDocument || iframe.contentWindow.document).getElementById("AliPay").click();
+}
+function showPayPal(){
+    var iframe = document.querySelector("#donate iframe");
+    (iframe.contentDocument || iframe.contentWindow.document).querySelector("#PayPal a").click();
+
 }
 function addBookmark(){
   var bookmark = document.createElement("span");
@@ -1665,3 +1717,28 @@ function inIframe () {
         return true;
     }
 }
+function mobilecheck() {
+  var check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
+};
+
+String.prototype.format = function()
+{
+    var args = arguments;
+    return this.replace(/\{(\d+)\}/g,                
+        function(m,i){
+            return args[i];
+        });
+}
+
+Array.prototype.move = function (old_index, new_index) {
+    if (new_index >= this.length) {
+        var k = new_index - this.length;
+        while ((k--) + 1) {
+            this.push(undefined);
+        }
+    }
+    this.splice(new_index, 0, this.splice(old_index, 1)[0]);
+    return this; // for testing purposes
+};
